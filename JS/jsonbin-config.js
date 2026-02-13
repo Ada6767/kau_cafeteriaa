6999b1147d4baced25ce3e0d61e38d5e8a76d92b
Ada6767 <sshafei0004@stu.kau.edu.sa>
2026-02-13 22:30:55 +0300

    jsonbin-config.js

--- JS/jsonbin-config.js
+++ JS/jsonbin-config.js
@@ -10,9 +10,13 @@
 
 // This file is loaded as a plain <script> tag so it sets window globals
 window.JSONBIN_CONFIG = {
-    API_KEY: "$2a$10$8aAU3ZjrF09kvBzqGZCwG.gkJ3qY9uugDrm.9LQ7H6rnTsvqVpU..",
-    BIN_ID:  "698f3d6dae596e708f282537",   // the bin you already created
-    DATA_BIN_ID: "698f7917ae596e708f2894f6"    // new bin for users & tickets
+  window.JSONBIN_CONFIG = {
+    API_KEY:     "$2a$10$8aAU3ZjrF09kvBzqGZCwG.gkJ3qY9uugDrm.9LQ7H6rnTsvqVpU..",
+    MENU_BIN_ID: "698f3d6dae596e708f282537",
+    DATA_BIN_ID: "698f7917ae596e708f2894f6"
+};
+
+export const JSONBIN_CONFIG = window.JSONBIN_CONFIG;
 };
 
 // Also export for ES module pages (admin, index)
